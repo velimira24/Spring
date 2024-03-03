8d55086a0af9bf561bdf9c71d112638425db57fb
Velimira <revekka-best@mail.ru>
2024-03-03 15:46:31 +0300

    HomeWork

---
+++ lesson2/src/test/Test.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
